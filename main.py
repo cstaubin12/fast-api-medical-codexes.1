b68f966e5ce86abbadb208d00a6c3284a9e28f73
cstaubin12 <colin.staubin@stonybrook.edu>
2023-05-02 19:40:55 +0000

    added data and main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import pandas as pd
+
+
+df = pd.read_csv('./data/diagnoses.2019.csv')
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['get'])
+def home():
+    return 'this is an api service for MN ICD code details'
+
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(10)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/icd/<value>', methods=["GET"])
+def icd(value):
+    filtered = df[df['principal_diagnosis_code'] == value]
+    return filtered.to_json(orient="records")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
